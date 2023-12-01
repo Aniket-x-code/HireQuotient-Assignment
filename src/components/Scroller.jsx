3a2a2593e55968ea8b46d1a038099800faf8f606
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-12-02 04:18:42 +0530

    adding delay to animation

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import fr from '../assets/fr.png';
-import 'aos/dist/aos.css'
+import 'aos/dist/aos.css';
 
 const Scroller = () => {
+  useEffect(() => {
+    const initializeAOS = () => {
+      AOS.init({
+        duration: 2000
+      });
+    };
 
-    useEffect(() => {
-        AOS.init({
-          duration : 2000
-        });
-      },[])
-    
+    // Adding a delay to avoid the AOS animation on page load
+    const delay = 100;
+    const timeoutId = setTimeout(initializeAOS, delay);
 
-      
+    // Clear the timeout when the component unmounts to avoid memory leaks
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className=' mx-28 py-6 ' data-aos="fade-up"
-    data-aos-duration="3000">
-    <img className='animation'  src={fr}  alt="img" />
+    <div className='mx-28 py-6'>
+      <img className='animation' data-aos="zoom-in" src={fr} alt="img" />
     </div>
-  )
-}
+  );
+};
 
-export default Scroller
+export default Scroller;
