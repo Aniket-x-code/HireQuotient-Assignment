544f5b31414490ba044f75ada5c8d2d2227b190b
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-12-02 04:16:43 +0530

    added aos animation

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect } from 'react'
 import AOS from 'aos';
-import fr from '../assets/fr.png'
+import fr from '../assets/fr.png';
+import 'aos/dist/aos.css'
+
 const Scroller = () => {
+
+    useEffect(() => {
+        AOS.init({
+          duration : 2000
+        });
+      },[])
+    
+
+      
   return (
-    <div className=' mx-28 py-6 '>
-    <img className='max-h-full max-w-full ' src={fr}  alt="img" />
+    <div className=' mx-28 py-6 ' data-aos="fade-up"
+    data-aos-duration="3000">
+    <img className='animation'  src={fr}  alt="img" />
     </div>
   )
 }
